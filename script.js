ef6e0455e6d424ced3c701f786ca5898967a3a09
Carlos <9671918@dev.azure.com>
2021-04-06 09:21:23 -0300

    Commit inicial

--- script.js
+++ script.js
@@ -10,6 +10,47 @@ const classNames = {
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   
+  var count = 0;
+
   function addTodo() {
-    alert('Boton Add TODO clickeado!')
+    //alert('Boton Add TODO clickeado!')
+    var tituloTarea = pedirInfo("Ingrese el nombre de la tarea");
+
+    if(tituloTarea != undefined && tituloTarea.trim() != ''){
+      var item = document.createElement("input");
+      item.type = "checkbox"
+      item.name = "checkbox";
+      item.addEventListener('change',tareasUncount);
+
+      var label = document.createElement("label");
+      label.innerText = tituloTarea;
+      label.prepend(item);
+
+      list.append(label);
+      list.append(document.createElement("br"));
+
+      itemCountSpan.innerText = ++count;
+      tareasUncount()
+    }else{
+      alert("Intente de nuevo");
+    }
+
+
+  }
+
+  function pedirInfo(info){
+    return prompt(info);
+  }
+
+  function tareasUncount(){
+    let checks = document.getElementsByName("checkbox");
+    let uncheck = 0;
+    
+    for (let index = 0; index < checks.length; index++) {
+      if(!checks[index].checked){
+        uncheck++;
+      }
+      
+    }
+    uncheckedCountSpan.innerText = uncheck;
   }
